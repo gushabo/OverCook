60ab6024f6211cf178f294efd093bf130176b397
gushabo <113316441+gushabo@users.noreply.github.com>
2025-04-08 18:05:16 -0600

    Adding VR and Beginning the player
    We added the package of VR, autohand, and we start building the player.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speedMovement;
+
+    private void Start() {
+        speedMovement = 15f;
+    }
+
+    private void Update() { }
+
+
+
+}
